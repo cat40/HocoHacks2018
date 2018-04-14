3ac26bfe1efbb675e0e6fb4b5bdb23c67d590d3d
zerostarm <zerostarm@users.noreply.github.com>
2018-04-14 13:12:53 -0400

    input finished?

--- Input.py
+++ Input.py
@@ -5,17 +5,51 @@ Created on Apr 14, 2018
 '''
 import cPickle as pickle
 
+
 def takeInput():
-    try: attributes = pickle.load('config')
+    try:
+        with open('config') as f: 
+            attributes = pickle.load(f)
     except IOError: pass
     else: return attributes
-        return
     attributes = dict()
+    
     print "This is a program that checks if google has updated information on yourself"
-    print "Input a list of activities, addresses etc. \n(use 'end' to stop inputting parameters)")
-    for par in ('names', 'activities', 'addresses'):
-        attributes[par] = #input thing
-    pickle.dump(attributes, 'file')
+    print "Input a list of names, activities, addresses etc. \n(use 'end' to stop inputting parameters)"
+    
+    nameList = []
+    activitiesList = []
+    addressesList = []
+    i = 1
+    while True :
+        inp = raw_input("name " + str(i) + ": ")
+        if(inp == "end"):
+            break
+        else:
+            nameList.append(input)
+        i += 1
+    i = 1
+    while True :
+        inp = raw_input("addresses " + str(i) + ": ")
+        if(inp == "end"):
+            break
+        else:
+            addressesList.append(input)
+        i += 1
+    i = 1
+    while True :
+        inp = raw_input("activities " + str(i) + ": ")
+        if(inp == "end"):
+            break
+        else:
+            activitiesList.append(input)
+        i += 1
+    attributes["name"] = nameList
+    attributes["activities"] = activitiesList   
+    attributes["addresses"] = addressesList 
+    
+    with open('config', 'w') as f:
+        pickle.dump(attributes, f)
     return attributes
 
 
@@ -23,7 +57,3 @@ if __name__ == '__main__':
     takeInput()
     file = open("config", "r")
     print(file.readlines())
-
-
-urls = search()
-return zip(urls, (htmlget(url) for url in urls))
